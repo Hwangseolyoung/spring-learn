f54847e73fbd94e8aa1e096c5538d45a3d529dc4
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-05-12 17:54:14 +0900

    list customer

--- src/main/java/org/zerock/domain/ex01/EmployeeDto.java
+++ src/main/java/org/zerock/domain/ex01/EmployeeDto.java
@@ -2,13 +2,21 @@ package org.zerock.domain.ex01;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import lombok.Data;
 
 @Data
 public class EmployeeDto {
 	
+	private int id;
 	private String lastName;
 	private String firstName;
+	
+	// input type="date" 
+//	@DateTimeFormat(pattern = "yyyy-MM-dd") // 책 135
+	@DateTimeFormat(iso = ISO.DATE) // 국제표준 날짜 형식
 	private LocalDate birthDate;
 	private String Photo;
 	private String Notes;
