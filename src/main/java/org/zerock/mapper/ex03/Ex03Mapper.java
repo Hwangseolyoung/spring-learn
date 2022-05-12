5e8023480946ca23c2b147693906b3bc06cbd491
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-05-12 17:55:26 +0900

    update

---
+++ src/main/java/org/zerock/mapper/ex03/Ex03Mapper.java
@@ -0,0 +1,21 @@
+package org.zerock.mapper.ex03;
+
+import java.util.List;
+
+import org.zerock.domain.ex02.BoardDto;
+
+public interface Ex03Mapper {
+	// mapper interface와 동일한 이름의 xml파일을 만들어준다.
+	// 쿼리는 xml에 작성한다.
+	String selectCustomerNameById(int id);
+	
+	String selectEmployeeFirstNameById(int id);
+
+	List<BoardDto> selectBoard();
+	
+	BoardDto getBoard(int id);
+
+	int updateBoard(BoardDto board);
+
+
+}
